46cb475f3a60c2e0c196a2c0ad568c935fa59a10
Youko Tada <counter0430culture@yahoo.co.jp>
2017-02-11 08:06:02 +0000

    created user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -17,10 +19,27 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+      flash[:success] = "Profile successfully updated!" 
+    else
+      render 'edit'
+    end
+  end
+
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location, :introduction)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
   end
 end
-
